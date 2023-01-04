ebdc4d39333cccfebcd0d9627da8a92475bb8982
victor291201 <victor291201hugo@gmail.com>
2023-01-04 16:00:32 -0500

    initial version

---
+++ script.js
@@ -0,0 +1,12 @@
+function clic1(){
+    document.getElementById("l1").classList.toggle("disactive")
+}
+function clic2(){
+    document.getElementById("l2").classList.toggle("disactive")
+}
+function clic3(){
+    document.getElementById("l3").classList.toggle("disactive")
+}
+function clic4(){
+    document.getElementById("l4").classList.toggle("disactive")
+}
